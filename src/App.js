0e6dcac8c9d6d905593dac67cec020757f021a98
mafalda-coelho <mafa.coel1@gmail.com>
2024-05-22 13:07:14 +0100

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+
+        <Weather />
+
         <footer>
           This project was coded by{" "}
           <a href="https://www.linkedin.com/in/mafalda-coelho-5bb15a263/">
