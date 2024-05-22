be05d1e5ec923a31c1636410ffa3ba91329300f5
mafalda-coelho <mafa.coel1@gmail.com>
2024-05-22 11:40:25 +0100

    created react project and added links to my profiles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/mafalda-coelho-5bb15a263/"
+          target="_blank"
+        >
+          Mafalda Coelho
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/mafalda-coelho/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github,
+        </a>{" "}
+        hosted on{" "}
+        <a href="https://celadon-bavarois-3c8009.netlify.app" target="_blank">
+          Netlify🖤
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
