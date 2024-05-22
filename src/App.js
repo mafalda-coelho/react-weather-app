a97e4f0e75c37c91ce120543e5ebb6021579f0ca
mafalda-coelho <mafa.coel1@gmail.com>
2024-05-22 12:07:45 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,27 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/mafalda-coelho-5bb15a263/"
-          target="_blank"
-        >
-          Mafalda Coelho
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/mafalda-coelho/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github,
-        </a>{" "}
-        hosted on{" "}
-        <a href="https://celadon-bavarois-3c8009.netlify.app" target="_blank">
-          Netlify🖤
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/mafalda-coelho-5bb15a263/">
+            Mafalda Coelho
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/mafalda-coelho/react-weather-app">
+            open-sourced on Github,
+          </a>{" "}
+          hosted on{" "}
+          <a href="https://celadon-bavarois-3c8009.netlify.app">Netlify🖤</a>
+        </footer>
+      </div>
     </div>
   );
 }
